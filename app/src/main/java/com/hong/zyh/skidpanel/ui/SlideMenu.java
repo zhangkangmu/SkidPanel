f68d187e6ebb6e846b1da3238bd92248da1c228d
zhangyuhong <289590351@qq.com>
2019-03-02 02:15:49 +0800

    解决侧滑面板上下滑动的时候也会左右滑动bug，方法：增加触摸事件的戒拦判断

--- app/src/main/java/com/hong/zyh/skidpanel/ui/SlideMenu.java
+++ app/src/main/java/com/hong/zyh/skidpanel/ui/SlideMenu.java
@@ -24,8 +24,8 @@ import android.widget.Scroller;
  */
 
 public class SlideMenu extends ViewGroup {
-
-    private float downX;
+    private float downX; // 按下的x坐标
+    private float downY; // 按下的y坐标
     private float moveX;
     public static final int MAIN_STATE = 0;
     public static final int MENU_STATE = 1;
@@ -213,5 +213,36 @@ public class SlideMenu extends ViewGroup {
         }else {
             close();
         }
+    }
+
+    /**
+     * 拦截判断
+     */
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = ev.getX();
+                downY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                float xOffset = Math.abs(ev.getX() - downX);
+                float yOffset = Math.abs(ev.getY() - downY);
+
+                if(xOffset > yOffset && xOffset > 5){ // 水平方向超出一定距离时,才拦截
+                    return true; // 拦截此次触摸事件, 界面的滚动
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+
+            default:
+                break;
+        }
+        return super.onInterceptTouchEvent(ev);
     }
 }
